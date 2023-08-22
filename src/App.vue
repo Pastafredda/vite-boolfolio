7de849ae34f0122792d09828f2036620aed5bad0
Pastafredda <justspotify09@gmail.com>
2023-08-22 19:13:08 +0200

    cards in pagina

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import BookCard from './components/BookCard.vue';
+import axios from 'axios';
+
+
+export default {
+  components: {
+    BookCard
+  },
+  data() {
+    return {
+      books: []
+    }
+  },
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/v1/book-index')
+      .then(response => {
+        const data = response.data
+        this.books = data.books
+        console.log(this.books);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <BookCard :books="books" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/BookCard.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+
+    props: {
+        books: Object
+    }
+
+}
+
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li class="card" v-for="book in books" :key="book.id">{{ book.title }}</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 50px;
+}
+
+.card {
+    background-color: rgb(4, 109, 127);
+    margin: 10px;
+    color: white;
+    height: 300px;
+    width: calc(100% / 11);
+    text-align: center;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+
+}
+</style>
